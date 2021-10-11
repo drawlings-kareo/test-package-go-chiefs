d1db228bb05531cdc66cf597e120120f7fdab064
David Rawlings <david.rawlings@kareo.com>
2021-10-11 12:30:26 -0700

    fix: removing lots of code

--- index.js
+++ index.js
@@ -3,19 +3,3 @@
 export default function visitArrowhead() {
   console.log("Go Kansas City Chiefs!");
 }
-
-export function getScore() {
-  return "21 - 17";
-}
-
-export function getAnotherScore() {
-  return "24 - 17";
-}
-
-export function getFinalScore() {
-  return "31 - 20";
-}
-
-export function getNewScore() {
-  return "0 - 0";
-}
