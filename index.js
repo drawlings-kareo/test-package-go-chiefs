5c2eb0056cdb196201d9fd7b57cf69b3f74766d9
David Rawlings <david.rawlings@kareo.com>
2021-10-11 12:47:38 -0700

    fix: set returned string to empty

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 #!/usr/bin/env node
 
 export default function getScore() {
-  console.log("Chiefs " + getChiefsScore() + ", Rams" + getRamsScore());
-}
-
-function getChiefsScore() {
-  return 51;
-}
-
-function getRamsScore() {
-  return 54;
+  console.log("");
 }
